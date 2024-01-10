8ed5abde4a139861b0294ba29f1d1d13df3e973e
Alex-Pablo <alexpabloanibal@gmail.com>
2024-01-09 20:01:22 -0600

    add menu drinks

--- src/app/components/drinks/drinks.component.ts
+++ src/app/components/drinks/drinks.component.ts
@@ -1,12 +1,94 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+export interface IDrink {
+  title:string
+  color:string
+  drinks: IDrinkPrice[]
+}
+
+export interface IDrinkPrice {
+  drink: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-drinks',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './drinks.component.html',
   styleUrl: './drinks.component.scss'
 })
 export class DrinksComponent {
+  drinks: IDrink[]= [
+    //host drink
+    {
+      title:'Bebidas Calientes',
+      color:'#FBFBF3',
+      drinks:[
+        {
+          drink:'Macchiato/Cortado',
+          price:15
+        },
+        {
+          drink:'Americano',
+          price:15
+        },
+        {
+          drink:'Flat White',
+          price:20
+        }
+      ]
+    },
 
+    //cold drinks
+    {
+      title:'Bebidas Frias',
+      color:'#FAFAFA',
+      drinks:[
+        {
+          drink:'Iced Coffee',
+          price:20,
+        },
+        {
+          drink:'Iced Latte',
+          price:22
+        },
+        {
+          drink:'Caramelate Frio',
+          price:23
+        }
+      ]
+    },
+    //frappes
+    {
+      title:'Frappe',
+      color:'F9F6EE',
+      drinks:[
+        {
+          drink:'Frappe de caramelate',
+          price:23
+        },
+        {
+          drink:'Frappe de chocolate',
+          price:25
+        }
+      ]
+    },
+    //smoothies
+    {
+      title:'Smoothies',
+      color:'FFFAFA',
+      drinks:[
+        {
+          drink:'Smoothie  verde',
+          price: 25
+        },
+        {
+          drink:'Smoothie chocolate',
+          price:25
+        }
+      ]
+    }
+  ]
 }
--- src/app/view/menu/menu.component.ts
+++ src/app/view/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
