4c6a4ed5ffe7c2f227e144e40aa10086968bedff
Alex-Pablo <alexpabloanibal@gmail.com>
2024-01-09 18:54:58 -0600

    create the structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { MenuComponent } from './view/menu/menu.component';
+import { DrinksComponent } from './components/drinks/drinks.component';
+import { FoodComponent } from './components/food/food.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'menu',
+        component:MenuComponent,
+        children:[
+            {
+                path:'',
+                redirectTo:'bebidas',
+                pathMatch:'full'
+            },
+            {
+                path:'bebidas',
+                component: DrinksComponent
+            },
+            {
+                path:'brunch',
+                component:FoodComponent
+            }
+        ]
+    },
+    {
+        path:'**',
+        component: HomeComponent
+    }
+];
